b9e198dea5e1f4d6395448c53dbf6ec96c2602ab
sooryaprabhath <sooryaprabhath.poojary@niveussolutions.com>
2023-06-02 16:09:15 +0530

    bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { InfinitySpin } from 'react-loader-spinner';
 
 const App = () => {
   const [sentence, setSentence] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
 
   const generateSentence = async () => {
-    try {
-      const response = await fetch("https://mystic-vision-be-hpur-b2lafqs24-sooryaprabhath.vercel.app/random-sentence");
-      const data = await response.json();
-      setSentence(data.sentence);
-    } catch (error) {
-      console.log(error);
+    setSentence("");
+    if (clickCount === 0) {
+      setIsClicked(true);
+      try {
+        const response = await fetch(
+          "https://mystic-vision-be-hpur-b2lafqs24-sooryaprabhath.vercel.app/random-sentence"
+        );
+        const data = await response.json();
+        setSentence(data.sentence);
+        setClickCount(-1);
+      } catch (error) {
+        console.log(error);
+      }
+      setTimeout(() => {
+        setIsClicked(false);
+      }, 10000);
+    } else {
+      setIsClicked(true);
+      setTimeout(async () => {
+        try {
+          const response = await fetch(
+            "https://mystic-vision-be-hpur-b2lafqs24-sooryaprabhath.vercel.app/random-sentence"
+          );
+          const data = await response.json();
+          setSentence(data.sentence);
+        } catch (error) {
+          console.log(error);
+        }
+      }, 1000); // debounce time of 1 second
+      setTimeout(() => {
+        setIsClicked(false);
+      }, 10000);
     }
   };
 
@@ -22,9 +51,13 @@ const App = () => {
         <div class="wave"></div>
       </div>
       <h1 className="title">Your Fortune Today is:</h1>
-      <button className="generate-button" onClick={generateSentence}>
-        Generate
-      </button>
+      {!isClicked ? (
+        <button className="generate-button" onClick={generateSentence}>
+          Generate
+        </button>
+      ) : (
+        <InfinitySpin width="200" color="#4fa94d" />
+      )}
       {sentence && <p className="generated-sentence">{sentence}</p>}
     </div>
   );
