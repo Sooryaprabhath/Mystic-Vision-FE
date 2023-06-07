8d98422d9a1827bd693c08e824e09dfe8349c6e7
sooryaprabhath <sooryaprabhath.poojary@niveussolutions.com>
2023-06-07 15:07:22 +0530

    3rd party api

--- src/App.js
+++ src/App.js
@@ -1,65 +1,58 @@
 import React, { useState } from "react";
 import "./App.css";
-import { InfinitySpin } from 'react-loader-spinner';
+import { InfinitySpin } from "react-loader-spinner";
 
 const App = () => {
+  const url =
+    "https://fortune-cookie2.p.rapidapi.com/fortune/by_category?category_key=general_fortune";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": '766b625b43mshba3009882d7692fp175232jsn25c285ab420f',
+      "X-RapidAPI-Host": "fortune-cookie2.p.rapidapi.com",
+    },
+  };
+
   const [sentence, setSentence] = useState("");
   const [isClicked, setIsClicked] = useState(false);
   const [clickCount, setClickCount] = useState(0);
 
   const generateSentence = async () => {
     setSentence("");
-    if (clickCount === 0) {
-      setIsClicked(true);
-      try {
-        const response = await fetch(
-          "https://mystic-vision-be-hpur-b2lafqs24-sooryaprabhath.vercel.app/random-sentence"
-        );
-        const data = await response.json();
-        setSentence(data.sentence);
-        setClickCount(-1);
-      } catch (error) {
-        console.log(error);
-      }
-      setTimeout(() => {
-        setIsClicked(false);
-      }, 10000);
-    } else {
-      setIsClicked(true);
-      setTimeout(async () => {
-        try {
-          const response = await fetch(
-            "https://mystic-vision-be-hpur-b2lafqs24-sooryaprabhath.vercel.app/random-sentence"
-          );
-          const data = await response.json();
-          setSentence(data.sentence);
-        } catch (error) {
-          console.log(error);
-        }
-      }, 500);
-      setTimeout(() => {
-        setIsClicked(false);
-      }, 10000);
+    setIsClicked(true);
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setSentence(data.answer);
+      setClickCount(-1);
+    } catch (error) {
+      console.log(error);
     }
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 10000);
   };
 
   return (
-   <div className='wrapper'> <div className="container">
-   <div>
-     <div className="wave"></div>
-     <div className="wave"></div>
-     <div className="wave"></div>
-   </div>
-   <h1 className="title">Your Fortune Today is:</h1>
-   {!isClicked ? (
-     <button className="generate-button" onClick={generateSentence}>
-       Generate
-     </button>
-   ) : (
-     <InfinitySpin width="200" color="#4fa94d" />
-   )}
-   {sentence && <p className="generated-sentence">{sentence}</p>}
- </div></div>
+    <div className="wrapper">
+      {" "}
+      <div className="container">
+        <div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+        </div>
+        <h1 className="title">Your Fortune Today is:</h1>
+        {!isClicked ? (
+          <button className="generate-button" onClick={generateSentence}>
+            Generate
+          </button>
+        ) : (
+          <InfinitySpin width="200" color="#4fa94d" />
+        )}
+        {sentence && <p className="generated-sentence">{sentence}</p>}
+      </div>
+    </div>
   );
 };
 
